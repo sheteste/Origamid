f29a42e5f34307c9cf2b73f02096008794850628
Shetest <ellen.santtis@gmail.com>
2022-01-08 16:47:05 -0300

    atividade 1 finalizada

--- javaScript/01-DOM/aula01.js
+++ javaScript/01-DOM/aula01.js
@@ -1,6 +1,15 @@
 //Retorne a url da página atual utilizando o objeto window
-
 let href = window.location.href;
 console.log(href);
 
-//
+//selecione o primeiro elemento da página que possua a classe ativa
+let elementoAtivio = document.querySelector('.ativo');
+console.log(elementoAtivio);
+
+//retorne a linguagem do navegador
+let idioma = window.navigator.language;
+console.log(idioma);
+
+//Retorne a largira da janela
+let largura = window.innerWidth;
+console.log(largura);
