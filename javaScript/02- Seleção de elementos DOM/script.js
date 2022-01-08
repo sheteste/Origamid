2a70d50640f0c4b310bc23ec9e6e34a751edb986
Shetest <ellen.santtis@gmail.com>
2022-01-08 20:09:32 -0300

    finalizando elementos e seleção

--- javaScript/02- Seleção de elementos DOM/script.js
+++ javaScript/02- Seleção de elementos DOM/script.js
@@ -1,5 +1,5 @@
 let animals = document.getElementById('animais');
-console.log(animals);
+// console.log(animals);
 
 //seleciona pela class, retorna um HTMLCollection
 let gridSection = document.getElementsByClassName('grid-section');
@@ -9,7 +9,7 @@ let contato = document.getElementsByClassName('grid-section contato');
 let ul = document.getElementsByTagName('ul');
 
 //Retorna o primeiro elemento
-console.log(gridSection[0]);
+// console.log(gridSection[0]);
 
 //QuerySelector retorna o primeiro elemento que combinar com o seu seletor CSS.(. é para classe e # para id);
 
@@ -30,4 +30,52 @@ let photoAnimals = document.querySelectorAll('.animais-lista img');
 
 
 //Retorna o segundo elemento
-console.log(gridSection2[1]);
+// console.log(gridSection2[1]);
+
+
+//Exemplos de htmlCollection e NodeCollection
+let gridSectionHtml = document.getElementsByClassName('grid-section');
+let gridSectionNode = document.querySelectorAll('.grid-section');
+
+//Adicionando uma classe a um elemento html
+firstUl.classList.add('grid-section')
+
+// console.log(gridSectionHtml);
+// console.log(gridSectionNode);
+
+//Array-Like: htmlCollection e NodeList são array-like, parecem uma array mas não são. O método Array forEach() por exemplo, existe apenas no NodeList.
+
+//É possível trasformar array-like em uma array real, utilizando o método Array.from()
+
+gridSectionNode.forEach(function(item, index){
+    // console.log(index);
+});
+
+//Transformando um array-like em array
+let arrayGrid = Array.from(gridSectionHtml);
+// console.log(arrayGrid);
+
+
+//Exercícios
+
+//Retorne no console todas as imagens do site
+
+let img = document.querySelectorAll('img');
+console.log(img);
+
+//Retorne no console apenas as imagens que começaram com a palavra imagem
+let beginImg = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(beginImg);
+
+
+//Selecione todos os links internos(onde o href começa com #)
+let allLinks = document.querySelectorAll('[href^="#"]')
+console.log(allLinks);
+
+//Selecione o primeiro h2 dentro de .animais-descricao
+let firstH2 = document.querySelector('.animais-descricao h2');
+console.log(firstH2);
+
+//Selecione o último p do site
+let lastP = document.querySelectorAll('p');
+console.log(lastP[lastP.length -1]);
